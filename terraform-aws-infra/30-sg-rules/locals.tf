6e589820fced28172a4a8b230767b5276e23d1da
Akhil Naidu <akhilnaid1997@gmail.com>
2025-10-29 19:11:21 +0530

    message

--- terraform-aws-infra/30-sg-rules/locals.tf
+++ terraform-aws-infra/30-sg-rules/locals.tf
@@ -1,5 +1,5 @@
 locals {
   bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
   backend_alb_id = data.aws_ssm_parameter.backend_alb_id.value
-  mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id
+  mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
 }
