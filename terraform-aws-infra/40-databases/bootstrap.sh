74459b724c11f8be456c6ef55ef572c646690d99
Akhil Naidu <akhilnaid1997@gmail.com>
2025-10-30 15:25:14 +0530

    message

--- terraform-aws-infra/40-databases/bootstrap.sh
+++ terraform-aws-infra/40-databases/bootstrap.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
 dnf install ansible -y
-ansible-pull -U https://github.com/akhilnaidu1997/ansible-roboshop-roles-tf.git -e component=mongodb main.yaml
+component=$1
+# ansible-pull -U https://github.com/akhilnaidu1997/ansible-roboshop-roles-tf.git -e component=mongodb main.yaml
+
+ANSIBLE_URL=https://github.com/akhilnaidu1997/ansible-roboshop-roles-tf.git
+REPO_DIR=/opt/roboshop/ansible
+ANSIBLE_DIR=ansible-roboshop-roles-tf
+
+mkdir -p $REPO_DIR
+mkdir -p /var/log/roboshop
+touch ansible.log
+
+cd $REPO_DIR
+
+if [ -d $ANSIBLE_DIR ]
+    cd $ANSIBLE_DIR
+    git pull
+else
+    git clone $ANSIBLE_URL
+
+ansible-playbook -e component=$component main.yaml
