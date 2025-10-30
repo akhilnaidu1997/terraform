25c39480422a8d3f19450ef40001feb7859aa3b9
Akhil Naidu <akhilnaid1997@gmail.com>
2025-10-30 15:34:26 +0530

    message

--- terraform-aws-infra/30-sg-rules/main.tf
+++ terraform-aws-infra/30-sg-rules/main.tf
@@ -24,3 +24,30 @@ resource "aws_security_group_rule" "mongodb" {
   security_group_id = local.bastion_sg_id
   source_security_group_id = local.mongodb_sg_id
 }
+
+resource "aws_security_group_rule" "redis" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  security_group_id = local.bastion_sg_id
+  source_security_group_id = local.redis_sg_id
+}
+
+resource "aws_security_group_rule" "rabbitmq" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  security_group_id = local.bastion_sg_id
+  source_security_group_id = local.mongodb_sg_id
+}
+
+resource "aws_security_group_rule" "mysql" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  security_group_id = local.bastion_sg_id
+  source_security_group_id = local.mysql_sg_id
+}
