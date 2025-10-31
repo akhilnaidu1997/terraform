2e1488117814960286c7b61acdf54e6de039970c
Akhil Naidu <akhilnaid1997@gmail.com>
2025-10-31 12:25:34 +0530

    message

--- terraform-aws-infra/40-databases/main.tf
+++ terraform-aws-infra/40-databases/main.tf
@@ -40,3 +40,89 @@ resource "terraform_data" "cluster" {
     ]
   }
 }
+
+resource "aws_instance" "redis" {
+  ami           = local.ami_id
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ local.redis_sg_id ]
+  subnet_id = local.database_subnet_ids
+
+  tags = merge(
+    var.database_tags,
+    local.common_tags,{
+        Name = "redis" # roboshop-dev 
+    }
+  )
+}
+
+resource "terraform_data" "redis" {
+  # Changes to any instance of the cluster requires re-provisioning
+  triggers_replace = [
+        aws_instance.redis.id
+  ]
+
+  # Bootstrap script can run on any instance of the cluster
+  # So we just choose the first in this case
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.redis.private_ip
+  }
+
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    # Bootstrap script called with private_ip of each node in the cluster
+    inline = [
+      "sudo chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh redis"
+    ]
+  }
+}
+
+resource "aws_instance" "rabbitmq" {
+  ami           = local.ami_id
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ local.rabbitmq_sg_id ]
+  subnet_id = local.database_subnet_ids
+
+  tags = merge(
+    var.database_tags,
+    local.common_tags,{
+        Name = "rabbitmq" # roboshop-dev 
+    }
+  )
+}
+
+resource "terraform_data" "redis" {
+  # Changes to any instance of the cluster requires re-provisioning
+  triggers_replace = [
+        aws_instance.rabbitmq.id
+  ]
+
+  # Bootstrap script can run on any instance of the cluster
+  # So we just choose the first in this case
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.rabbitmq.private_ip
+  }
+
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    # Bootstrap script called with private_ip of each node in the cluster
+    inline = [
+      "sudo chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh rabbitmq"
+    ]
+  }
+}
