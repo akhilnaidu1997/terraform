70822d05bc4ff07221a0e96994ed94e2e4ece79b
Akhil Naidu <akhilnaid1997@gmail.com>
2025-10-31 12:05:28 +0530

    message

--- terraform-aws-infra/40-databases/locals.tf
+++ terraform-aws-infra/40-databases/locals.tf
@@ -9,6 +9,6 @@ locals {
     Terraform = "True"
   }
   #ublic_subnet_ids = split(",", data.aws_ssm_parameter.b_subnet_id.value)[0]
-  database_subnet_ids = split(",", data.aws_ssm_parameter.database_subnet_id.value)[0]
+  database_subnet_ids = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
   mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.id
 }
