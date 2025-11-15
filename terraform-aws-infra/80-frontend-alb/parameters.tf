0f9d74ced05b06eb5df1da97e5a49bdc52e29778
Akhil Naidu <akhilnaid1997@gmail.com>
2025-11-15 11:48:00 +0530

    message

--- terraform-aws-infra/80-frontend-alb/parameters.tf
+++ terraform-aws-infra/80-frontend-alb/parameters.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "listener_arn" {
   name  = "/${var.project}/${var.environment}/listener_arn_frontend"
   type  = "String"
-  value = aws_lb_listener.backend_alb.arn
+  value = aws_lb_listener.front_end.arn
 }
