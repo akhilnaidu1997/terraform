b80f1b075f1ecb0798069199ea6325d4029c9594
Akhil Naidu <akhilnaid1997@gmail.com>
2025-11-15 11:37:20 +0530

    message

--- terraform-aws-infra/90-components/main.tf
+++ terraform-aws-infra/90-components/main.tf
@@ -1,6 +1,6 @@
 module "components" {
     for_each = var.components
-    source = "../terraform-roboshop-components"
+    source = "../../terraform-roboshop-components"
     component = each.key
     instance_type = var.instance_type
     priority = each.value.priority
